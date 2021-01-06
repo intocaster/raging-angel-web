7b054dc861abdbaf2e3ae7bf710a3d16d756d6cd
intocaster <76784229+intocaster@users.noreply.github.com>
2021-01-06 00:59:55 -0400

    Add files via upload

---
+++ send.php
@@ -0,0 +1,35 @@
+<?php
+$name = $_POST['name']
+$phone = $_POST['phone']
+$email = $_POST['email']
+$line1 = $_POST['line1']
+$line2 = $_POST['line2']
+$paypal = $_POST['paypal']
+
+// Datos para el correo
+$destinatario = "intocaster@gmail.com";
+$asunto = "Purchase Summary";
+
+$carta = "Customer: $nombre \n";
+$carta .= "email: $email \n";
+$carta .= "Phone Number: $phone \n";
+$carta .= "Address Line 1: $line1 \n";
+$carta .= "Address Line 2: $line2 \n";
+$carta .= "Transaction ID: $paypal \n";
+$header = "Sent from the Raging Angel store";
+// Enviando Mensaje
+mail($destinatario, $asunto, $carta, $header);
+echo "<script>alert('Your order has been successfully processed.'</script>"
+echo "<script>setTimeout(\"location.href='ragingangelshop.com'\",1000)</script>"
+
+
+
+
+
+
+
+
+
+
+
+?>
