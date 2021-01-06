22ed6c74cfa0f158b382ce1b5a243a83ef57d4cc
intocaster <76784229+intocaster@users.noreply.github.com>
2021-01-06 00:04:15 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ cerrar2.addEventListener('click', function(){
 window.addEventListener('click', function(e){
     console.log(e.target);
     if(e.target == flex2){
-        modal1.style.display = 'none';
+        modal2.style.display = 'none';
     }
 });
 let modal3 = document.getElementById('miModal3');
@@ -71,8 +71,25 @@ cerrar3.addEventListener('click', function(){
 window.addEventListener('click', function(e){
     console.log(e.target);
     if(e.target == flex3){
-        modal1.style.display = 'none';
+        modal3.style.display = 'none';
     }
 });
+let modal4 = document.getElementById('miModal4');
+let flex4 = document.getElementById('flex4');
+let abrir4 = document.getElementById('abrir4');
+let cerrar4 = document.getElementById('close4');
 
+abrir4.addEventListener('click', function(){
+    modal4.style.display = 'block';
+});
 
+cerrar4.addEventListener('click', function(){
+    modal4.style.display = 'none';
+});
+
+window.addEventListener('click', function(e){
+    console.log(e.target);
+    if(e.target == flex4){
+        modal4.style.display = 'none';
+    }
+});
